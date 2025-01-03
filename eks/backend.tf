084d844dfc8ab1125dd0509dc15c9ab702d12c8b
Reguraman Vellaichamy <reguramanvellaichamy@Reguramans-MBP.cust.communityfibre.co.uk>
2025-01-03 12:28:58 +0000

    first upload

---
+++ eks/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.9.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "my-eks-bucket-0902"
+    region         = "eu-north-1"
+    key            = "eks/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region  = var.aws-region
+}
