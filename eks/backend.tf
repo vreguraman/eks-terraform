311af265aef07064fd3a379372f50d894cfcadd6
vreguraman <96850237+vreguraman@users.noreply.github.com>
2025-01-03 17:16:02 +0000

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "my-eks-basket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "your-dynamodb-table"
+    dynamodb_table = "my-dynamodb-table"
     encrypt        = true
   }
 }
