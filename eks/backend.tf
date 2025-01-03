12d9cf523a5d1da43166c7c32f4b7eb8ac2d6fed
vreguraman <96850237+vreguraman@users.noreply.github.com>
2025-01-03 17:38:29 +0000

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,4 +1,4 @@
-terraform {
+/* terraform {
   required_version = "~> 1.10.3"
   required_providers {
     aws = {
@@ -6,7 +6,7 @@ terraform {
       version = "~> 5.49.0"
     }
   }
- }
+ } */
   /* backend "s3" {
     bucket         = "my-eks-basket"
     region         = "us-east-1"
